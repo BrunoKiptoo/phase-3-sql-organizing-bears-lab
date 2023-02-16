23c7a2834c80cd35badb16bff9c4de2f6995f52d
Bruno <brunochelimo@gmail.com>
2023-02-16 17:27:27 +0300

    done

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+  ('Yogi', 10, 'male', 'brown', 'grumpy', 1),
+  ('Paddington', 5, 'male', 'brown', 'friendly', 1),
+  ('Winnie', 3, 'female', 'golden', 'curious', 1),
+  ('Baloo', 15, 'male', 'grey', 'relaxed', 0),
+  ('Gummy', 1, 'female', 'purple', 'playful', 1),
+  ('Koda', 2, 'male', 'brown', 'energetic', 1),
+  ('Smokey', 20, 'male', 'grey', 'wise', 0),
+  (NULL, 4, 'female', 'black', 'mysterious', 1);
